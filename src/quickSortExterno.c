9646c33a09c0b8dfe2d11de28910e23afc7bed5f
octaviareika <ceciliaperet81@gmail.com>
2024-01-01 22:01:15 -0300

    mudando algumas coisas

--- src/quickSortExterno.c
+++ src/quickSortExterno.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef int TipoChave;
-typedef struct TipoItem {
-    TipoChave Chave;
-    /* outros componentes */
-} TipoItem;
+typedef long TipoChave;
+
+typedef struct TipoRegistro {
+    TipoChave chave;
+} TipoRegistro;
+
+typedef struct TipoPagina* TipoApontador;
+
+// struct de tipoArea
+
 typedef struct TipoArea {
-    TipoItem *Itens;
-    int Max;
-    int NumCelOcupadas;
+    TipoRegistro *r;
+    int n;
 } TipoArea;
-
 
 void QuickSortExterno(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int Dir){
     int i, j;
@@ -30,4 +34,94 @@ void QuickSortExterno(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, int Esq, int Di
     
 }
 
-// typedef de tipoArea
+void LeSup(FILE **ArqLEs, TipoRegistro *UltLido, int *Ls, short *OndeLer){
+    fseek(*ArqLEs, (*Ls - 1) * sizeof(TipoRegistro), SEEK_SET);
+    fread(UltLido, sizeof(TipoRegistro), 1, *ArqLEs);
+    (*Ls)--;
+    *OndeLer = false;
+}
+
+void LeInf(FILE **ArqLi, TipoRegistro *UltLido, int *Li, short *OndeLer){
+    fread(UltLido, sizeof(TipoRegistro), 1, *ArqLi);
+    (*Li)++;
+    *OndeLer = true;
+}
+
+void inserirArea(TipoArea *Area, TipoRegistro *UltLido, int *NRArea){
+    InsereItem(*UltLido, Area);
+    (*NRArea) = ObterNumeroCelulas(Area);
+}
+
+void EscreveMax(FILE **ArqLEs, TipoRegistro R, int *Es){
+    // Escreve o maior elemento da área no final do arquivo de saída
+    fseek(*ArqLEs, (*Es - 1) * sizeof(TipoRegistro), SEEK_SET);
+    fwrite(&R, sizeof(TipoRegistro), 1, *ArqLEs);
+    (*Es)--;
+}
+
+void EscreveMin(FILE **ArqEi, TipoRegistro R, int *Ei){
+    // Escreve o menor elemento da área no início do arquivo de saída
+    //fseek(*ArqEi, (*Ei - 1) * sizeof(TipoRegistro), SEEK_SET);
+    fwrite(&R, sizeof(TipoRegistro), 1, *ArqEi);
+    (*Ei)++;
+}
+
+void RetiraMax(TipoArea *Area, TipoRegistro *R, int *NRArea){
+    RetiraUltimo(Area, R);
+    (*NRArea) = ObterNumeroCelulas(Area);
+}
+
+void RetiraMin(TipoArea *Area, TipoRegistro *R, int *NRArea){
+    RetiraPrimeiro(Area, R);
+    (*NRArea) = ObterNumeroCelulas(Area);
+}
+
+
+void Particao(FILE **ArqLi, FILE **ArqEi, FILE **ArqLEs, TipoArea Area, int Esq, int Dir, int *i, int *j){
+    int Ls = Dir, Es = Dir, Li = Esq, Ei = Esq;
+    int NRArea = 0; // Número de elementos na área
+    short OndeLer = true;
+    TipoRegistro UltLido, R;
+    *i = Esq - 1;
+    *j = Dir + 1;
+    while (Li <= Ls){
+        if (ObterNumeroCelulas(&Area) == 0){
+            if (OndeLer){
+                LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
+            } else {
+                LeInf(ArqLi, &UltLido, &Li, &OndeLer);
+            }
+            inserirArea(&Area, &UltLido, &NRArea);
+        }
+        if (OndeLer){
+            RetiraMax(&Area, &R, &NRArea);
+            EscreveMax(ArqLEs, R, &Es);
+            if (R.chave < UltLido.chave){
+                UltLido = R;
+                *j = Es;
+                *i = Ei;
+            }
+        } else {
+            RetiraMin(&Area, &R, &NRArea);
+            EscreveMin(ArqEi, R, &Ei);
+            if (R.chave > UltLido.chave){
+                UltLido = R;
+                *i = Ei;
+                *j = Es;
+            }
+        }
+    }
+    while (Ei <= Es){
+        if (OndeLer){
+            LeSup(ArqLEs, &UltLido, &Ls, &OndeLer);
+        } else {
+            LeInf(ArqLi, &UltLido, &Li, &OndeLer);
+        }
+        if (UltLido.chave < R.chave){
+            EscreveMax(ArqLEs, R, &Es);
+            EscreveMin(ArqEi, UltLido, &Ei);
+        } else {
+            EscreveMax(ArqEi, UltLido, &Ei);
+            EscreveMin(ArqLEs, R, &Es);
+        }
+    }
