6efd27229a936ddd29eb5c1d315c02c3b44878ed
octaviareika <ceciliaperet81@gmail.com>
2024-01-03 15:05:17 -0300

    modificiando headers

---
+++ headers/quickSortExterno.h
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+
+
+#ifndef QUICKSORT_EXTERNO_H
+#define QUICKSORT_EXTERNO_H
+
+
+typedef struct TipoRegistro {
+    TipoChave chave;
+} TipoRegistro;
+
+typedef struct Contagem {
+    int comp; // numero de comparacoes
+    int transf; // numero de transferencias
+    int transfEscrita; // numero de transferencias de escrita
+    int transfLeitura; // numero de transferencias de leitura
+    int tempo; // tempo de execucao
+} Contagem;
+
+typedef struct TipoPagina* TipoApontador;
+
+// struct de tipoArea
+
+typedef struct TipoArea { // Tipo usado para representar a área de memória interna
+    TipoRegistro *r; // Vetor de registros
+    int n; // Número de registros armazenados
+} TipoArea;
+
+#endif
