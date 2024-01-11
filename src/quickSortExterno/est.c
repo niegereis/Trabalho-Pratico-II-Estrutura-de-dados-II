70b781fb5589de026f1e6ea60ace4ede2dadf1ac
GuilhermeSS <silvadesouzaguilherme242@gmail.com>
2024-01-11 00:53:59 -0300

    modificação no alunoLer

--- src/quickSortExterno/est.c
+++ src/quickSortExterno/est.c
@@ -11,14 +11,18 @@ Aluno AlunoLer(FILE *f) {
 
   fscanf(f, "%s %f %s ", aluno.numeroInscricao, &aluno.nota, aluno.estado);
   fgets(aluno.cidade, 50, f);
+  for(int i = 1; i < 50; i++){if(aluno.cidade[i] == ' ' && aluno.cidade[i-1] == ' '){aluno.cidade[i-1] = '\0';}} //remove os espaços em branco
   fgetc(f);
   fgets(aluno.curso, 30, f);
+  for(int i = 1; i < 30; i++){if(aluno.curso[i] == ' ' && aluno.curso[i-1] == ' '){aluno.curso[i-1] = '\0';}} //remove os espaços em branco
+
 
   return aluno;
 }
 
 int main(){
   FILE *a = fopen("PROVAO.TXT","r");
+  if(a == NULL){perror("error");}
   Aluno al;
 
   fseek(a, -100, 2);
