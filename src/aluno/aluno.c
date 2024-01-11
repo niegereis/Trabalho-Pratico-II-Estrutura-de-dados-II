57899af3e56b97e09a47f12516e08a00d09a2c62
GuilhermeSS <silvadesouzaguilherme242@gmail.com>
2024-01-11 00:54:17 -0300

    modificação no alunoLer

--- src/aluno/aluno.c
+++ src/aluno/aluno.c
@@ -23,8 +23,11 @@ Aluno AlunoLer(FILE *f) {
 
   fscanf(f, "%s %f %s ", aluno.numeroInscricao, &aluno.nota, aluno.estado);
   fgets(aluno.cidade, 50, f);
+  for(int i = 1; i < 50; i++){if(aluno.cidade[i] == ' ' && aluno.cidade[i-1] == ' '){aluno.cidade[i-1] = '\0';}} //remove os espaços em branco
   fgetc(f);
   fgets(aluno.curso, 30, f);
+  for(int i = 1; i < 30; i++){if(aluno.curso[i] == ' ' && aluno.curso[i-1] == ' '){aluno.curso[i-1] = '\0';}} //remove os espaços em branco
+
 
   return aluno;
 }
