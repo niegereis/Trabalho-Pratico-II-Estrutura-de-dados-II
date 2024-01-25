b045e3527f167bf6fa69c98b1c0e029c0455d678
Dudu Use <eduardo.silva@usemobile.xyz>
2024-01-25 09:42:35 -0300

    correcoes

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
   }
 
   int metodo, quantidade, situacao;
-  bool p = false;//váriavel para impressão no terminal
+  bool p = false; // váriavel para impressão no terminal
 
   metodo = atoi(argv[1]);
   quantidade = atoi(argv[2]);
@@ -39,18 +39,18 @@ int main(int argc, char **argv) {
   if (argc == 5 && (strcmp(argv[4], "-P") || strcmp(argv[4], "-p")))
     p = true;
 
-  //copia o arquivo selecionado pelo usuário para o output.txt, onde será ordenado
+  // copia o arquivo selecionado pelo usuário para o output.txt, onde será ordenado
   copiaArquivo(output, situacao, quantidade);
   fclose(output);
 
-  if (p){
+  if (p) {
     printf("\nAntes da ordenação:\n");
     imprimeTerminal(quantidade);
   }
 
   Analise analise;
   analise = ordenaMain(quantidade, metodo);
-  if (p){
+  if (p) {
     printf("\nDepois da ordenação:\n");
     imprimeTerminal(quantidade);
   }
